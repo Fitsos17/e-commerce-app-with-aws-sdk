9c56163c9c21e881e9daca913fe37ab9da8750e9
Nick <Nikolas.amiridis@gmail.com>
2022-09-26 22:39:49 +0300

    updated my app to use apigw and created a sample lambda function for testing.

--- lib/e-commerce-app-aws-course-stack.ts
+++ lib/e-commerce-app-aws-course-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import { RemovalPolicy } from "aws-cdk-lib";
+import { LambdaRestApi } from "aws-cdk-lib/aws-apigateway";
 import { AttributeType, BillingMode, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import {
@@ -8,8 +9,6 @@ import {
 } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
 export class ECommerceAppAwsCourseStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -38,5 +37,20 @@ export class ECommerceAppAwsCourseStack extends cdk.Stack {
     });
 
     productTable.grantReadWriteData(productFunction);
+
+    const apigw = new LambdaRestApi(this, "productApi", {
+      restApiName: "Product Service",
+      handler: productFunction,
+      proxy: false,
+    });
+
+    const product = apigw.root.addResource("product");
+    product.addMethod("GET"); // GET /product
+    product.addMethod("POST"); // GET /product
+
+    const singleProduct = product.addResource("{id}"); // /product/{id}
+    singleProduct.addMethod("GET"); // GET /product/{id}
+    singleProduct.addMethod("PUT"); // PUT /product/{id}
+    singleProduct.addMethod("DELETE"); // DELETE /product/{id}
   }
 }
